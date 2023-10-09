7320b178c210a11277da8e9f1e3b6b9b4e0f5e06
prabh <prabh@LAPTOP-OCRFH6B2>
2023-10-09 17:31:33 +0530

    hi

---
+++ Core_Java/src/uno/Data_Conversation_To_UpperCase.java
@@ -0,0 +1,16 @@
+package uno;
+
+import java.util.*;
+
+public class Data_Conversation_To_UpperCase {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the string data that you want to covert into uppercase:");//i love java
+		String str =sc.nextLine();
+		System.out.println("after converting into uppercase");
+		System.out.println(str.toUpperCase());//I LOVE JAVA
+
+	}
+}
