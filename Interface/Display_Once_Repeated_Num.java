628d89c222167702abccf5ec96cfabc643c915d1
Prabhuthvam <98016401+Prabhuthvam@users.noreply.github.com>
2023-11-09 19:20:09 +0530

    Add files via upload

---
+++ Interface/Display_Once_Repeated_Num.java
@@ -0,0 +1,33 @@
+package interfaces;
+
+public class Display_Once_Repeated_Num {
+
+	public static void main(String[] args) {
+		//Given an array where every element occurs three times, except one element which occurs only once.
+				//Find the element that occurs once.
+				//Input: arr[] = {12, 1, 12, 3, 12, 1, 1, 2, 3, 3} Output: 2
+
+				int arr[]= {12,1,12,3,12,1,1,2,3,3};
+				int eachInt;
+				int count=0;
+				int res=0;
+
+				for (int i = 0; i < arr.length; i++) {
+						count=0;
+					for (int j = 0; j < arr.length; j++) {
+						eachInt=arr[j];
+						
+						if (arr[i]==arr[j]){
+							count++;
+						}
+					}
+					if (count==3)continue;
+					else if (count==1) {
+						res=arr[i];
+					}
+				}
+				System.out.println("Once Repeated Number is "+res);
+			}
+		
+	}
+
---
+++ Interface/Swaping_Unsign_Int_Bits.java
@@ -0,0 +1,27 @@
+package interfaces;
+
+public class Swaping_Unsign_Int_Bits {
+
+	public static void main(String[] args) {
+		/*
+		 * Given an unsigned integer, swap all 0 bits with 1 bits and 1 bit to 0. For example, 
+		 * if the given number is 23 (10111),
+		 * then 1000---8
+		 */
+		int n =23,r,p=1,bn=0,res =0,p2=1;
+		
+		while( n != 0) {
+			r = n % 2;
+			n= n / 2;
+			if( r == 0) r=1;
+			else if(r==1) r=0;
+			res = res + r * p2;
+			bn = bn +r * p;
+			p = p * 10;
+			p2 = p2 * 2;
+		}
+		System.out.println("Binary number = "+bn);
+		System.out.println("Result = "+res);
+	}
+
+}
